rosslavni/jsdom

commit 0a074ef6311c20ee5c35196a28abcf0e980adcf9

Fix #1281: reset canvas when resizing it

lib/jsdom/level2/html.js
@@ -1473,6 +1473,12 @@ define('HTMLCanvasElement', {
     }
   },
   proto: {
+    _attrModified(name, value, oldValue) {
+      if ((name == "width" || name === "height") && this._nodeCanvas) {
+        const Canvas = require("canvas");
+        this._nodeCanvas = new Canvas(this.width, this.height);
+      }
+    },
     getContext(contextId) {
       if (this._nodeCanvas) {
         return this._nodeCanvas.getContext(contextId) || null;

test/living-html/htmlcanvaselement.js
@@ -1,5 +1,8 @@
 "use strict";
+const fs = require("fs");
+const path = require("path");
 const jsdom = require("../..");
+
 // Tests for the HTML canvas element
 // Spec: https://html.spec.whatwg.org/multipage/scripting.html#the-canvas-element
 
@@ -62,6 +65,37 @@ exports["canvas width and height must parse correctly initially (GH-1025)"] = t 
   const document = jsdom.jsdom("<canvas width='99' height='101'></canvas>");
   const canvas = document.querySelector("canvas");
 
+  t.strictEqual(canvas.width, 99);
+  t.strictEqual(canvas.height, 101);
+  t.done();
+};
+
+exports["canvas must resize correctly when given a non-default width/height (GH-1025)"] = t => {
+  const document = jsdom.jsdom("<canvas width='400' height='400'></canvas>");
+  const canvas = document.querySelector("canvas");
+  const ctx = canvas.getContext("2d");
+
+  ctx.beginPath();
+  ctx.strokeStyle = "rgba(0,255,0,1)";
+  ctx.moveTo(50, 50);
+  ctx.lineTo(50, 300);
+  ctx.lineTo(300, 300);
+  ctx.lineTo(300, 50);
+  ctx.lineTo(50, 50);
+  ctx.stroke();
+  ctx.closePath();
+
+  const expected = fs.readFileSync(path.resolve(__dirname, "files/expected-canvas.txt"), { encoding: "utf-8" }).trim();
+  t.strictEqual(canvas.toDataURL(), expected);
+  t.done();
+};
+
+exports["canvas width and height properties must reflect their attributes after setting them (GH-1281)"] = t => {
+  const document = jsdom.jsdom("<canvas></canvas>");
+  const canvas = document.querySelector("canvas");
+
+  canvas.setAttribute("width", 99);
+  canvas.setAttribute("height", 101);
   t.strictEqual(canvas.width, 99);
   t.strictEqual(canvas.height, 101);
   t.done();

test/living-html/files/expected-canvas.txt
@@ -1,0 +1,1 @@
+data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAZAAAAGQCAYAAACAvzbMAAAABmJLR0QA/wD/AP+gvaeTAAAEw0lEQVR4nO3YsW3DQBBFwaWhwliaSmNncqDEcPhA+gR6poKfHB72ZgAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAB+2VYPOM1rnqsnAJc7Zptj9QjevlYPOMU7HvviFcC19vHOP8pj9YATHbO5QuC2/DJ8nHtcIAD8OQEBIBEQABIBASAREAASAQEgERAAEgEBIBEQABIBASAREAASAQEgERAAEgEBIBEQABIBASAREAASAQEgERAAEgEBIBEQABIBASAREAASAQEgERAAEgEBIBEQABIBASAREAASAQEgERAAEgEBIBEQABIBASAREAASAQEgERAAEgEBIBEQABIBASAREAASAQEgERAAEgEBIBEQABIBASAREAASAQEgERAAEgEBIBEQABIBASAREAASAQEgERAAEgEBIBEQABIBASAREAASAQEgERAAEgEBIBEQABIBASAREAASAQEgERAAEgEBIBEQABIBASAREAASAQEgERAAEgEBIBEQABIBASAREAASAQEgERAAEgEBIBEQABIBASAREAASAQEgERAAEgEBIBEQABIBASAREAASAQEgERAAEgEBIBEQABIBASAREAASAQEgERAAEgEBIBEQABIBASAREAASAQEgERAAEgEBIBEQABIBASAREAASAQEgERAAEgEBIBEQABIBASAREAASAQEgERAAEgEBIBEQABIBASAREAASAQEgERAAEgEBIBEQABIBASAREAASAQEgERAAEgEBIBEQABIBASAREAASAQEgERAAEgEBIBEQABIBASAREAASAQEgERAAEgEBIBEQABIBASAREAASAQEgERAAEgEBIBEQABIBASAREAASAQEgERAAEgEBIBEQABIBASAREAASAQEgERAAEgEBIBEQABIBASAREAASAQEgERAAEgEBIBEQABIBASAREAASAQEgERAAEgEBIBEQABIBASAREAASAQEgERAAEgEBIBEQABIBASAREAASAQEgERAAEgEBIBEQABIBASAREAASAQEgERAAEgEBIBEQABIBASAREAASAQEgERAAEgEBIBEQABIBASAREAASAQEgERAAEgEBIBEQABIBASAREAASAQEgERAAEgEBIBEQABIBASAREAASAQEgERAAEgEBIBEQABIBASAREAASAQEgERAAEgEBIBEQABIBASAREAASAQEgERAAEgEBIBEQABIBASAREAASAQEgERAAEgEBIBEQABIBASAREAASAQEgERAAEgEBIBEQABIBASAREAASAQEgERAAEgEBIBEQABIBASAREAASAQEgERAAksfqASfa5zXP1SOAy+wzcyzewA93CcixegBwuWO8dQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAPgPvgHICQ78cINS9AAAAABJRU5ErkJggg==

commit 92bc03cded9d72fd4e55a934c346a29e315deaa3

Fix canvas fix
0a074ef6311c20ee5c35196a28abcf0e980adcf9 introduced a bug where canvas elements would not react correctly to non-width/height attribute changes. Oops.

lib/jsdom/level2/html.js
@@ -1478,6 +1478,8 @@ define('HTMLCanvasElement', {
         const Canvas = require("canvas");
         this._nodeCanvas = new Canvas(this.width, this.height);
       }
+
+      return core.HTMLElement.prototype._attrModified.apply(this, arguments);
     },
     getContext(contextId) {
       if (this._nodeCanvas) {

test/living-html/htmlcanvaselement.js
@@ -71,6 +71,10 @@ exports["canvas width and height must parse correctly initially (GH-1025)"] = t 
 };
 
 exports["canvas must resize correctly when given a non-default width/height (GH-1025)"] = t => {
+  if (!isCanvasInstalled(t)) {
+    return;
+  }
+
   const document = jsdom.jsdom("<canvas width='400' height='400'></canvas>");
   const canvas = document.querySelector("canvas");
   const ctx = canvas.getContext("2d");
@@ -102,6 +106,18 @@ exports["canvas width and height properties must reflect their attributes after 
 };
 
 exports["toDataURL should work (when the canvas npm package is provided) (GH-1025)"] = t => {
+  if (!isCanvasInstalled(t)) {
+    return;
+  }
+
+  const document = jsdom.jsdom("<canvas width='99' height='101'></canvas>");
+  const canvas = document.querySelector("canvas");
+
+  t.strictEqual(canvas.toDataURL().substring(0, 22), "data:image/png;base64,");
+  t.done();
+};
+
+function isCanvasInstalled(t) {
   /* eslint-disable global-require */
   let Canvas;
   try {
@@ -112,12 +128,8 @@ exports["toDataURL should work (when the canvas npm package is provided) (GH-102
   if (typeof Canvas !== "function") { // browserify will give back an empty object
     t.ok(true, "test ignored; not running with the canvas npm package installed");
     t.done();
-    return;
+    return false;
   }
 
-  const document = jsdom.jsdom("<canvas width='99' height='101'></canvas>");
-  const canvas = document.querySelector("canvas");
-
-  t.strictEqual(canvas.toDataURL().substring(0, 22), "data:image/png;base64,");
-  t.done();
-};
+  return true;
+}