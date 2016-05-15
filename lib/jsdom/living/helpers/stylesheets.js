rosslavni/jsdom

commit 780b5dc6de5d739ba10e9bfcb730d535d931f153

Remove mutation events (!)
This completely removes mutation events from jsdom. (Fixes #295, finally.)

Mutation events are no longer part of the DOM Standard, even though they are part of all existing browsers. They have been superceded by mutation observers.

Our hope was to implement mutation observers before removing mutation events. However, recent performance testing reveals that mutation events cause a *massive* slowdown of common DOM operations (like adding nodes or attributes, including during parsing). For example, compiling the ECMAScript draft spec is done using jsdom; compilation time goes from over a minute to less than thirty seconds with this change. Thus, it is worthwhile to remove mutation events even without their replacement in sight yet.

lib/jsdom/living/nodes/HTMLLinkElement-impl.js
@@ -67,20 +67,18 @@ function scanForImportRules(cssRules, baseUrl) {
 }
 
 class HTMLLinkElementImpl extends HTMLElementImpl {
-  constructor(args, privateData) {
-    super(args, privateData);
+  _attach() {
+    const wrapper = idlUtils.wrapperForImpl(this);
+    if (!/(?:[ \t\n\r\f]|^)stylesheet(?:[ \t\n\r\f]|$)/i.test(wrapper.rel)) {
+      // rel is a space-separated list of tokens, and the original rel types
+      // are case-insensitive.
+      return;
+    }
+    if (this.href) {
+      fetchStylesheet.call(this, this.href, this.sheet);
+    }
 
-    this.addEventListener("DOMNodeInsertedIntoDocument", () => {
-      const wrapper = idlUtils.wrapperForImpl(this);
-      if (!/(?:[ \t\n\r\f]|^)stylesheet(?:[ \t\n\r\f]|$)/i.test(wrapper.rel)) {
-        // rel is a space-separated list of tokens, and the original rel types
-        // are case-insensitive.
-        return;
-      }
-      if (this.href) {
-        fetchStylesheet.call(this, this.href, this.sheet);
-      }
-    });
+    super._attach();
   }
 
   get _accept() {

lib/jsdom/living/nodes/HTMLStyleElement-impl.js
@@ -68,23 +68,21 @@ function scanForImportRules(cssRules, baseUrl) {
 }
 
 class HTMLStyleElementImpl extends HTMLElementImpl {
-  constructor(args, privateData) {
-    super(args, privateData);
+  _attach() {
+    if (this.type && this.type !== "text/css") {
+      return;
+    }
 
-    this.addEventListener("DOMNodeInsertedIntoDocument", () => {
-      if (this.type && this.type !== "text/css") {
-        return;
+    let content = "";
+    for (const child of domSymbolTree.childrenIterator(this)) {
+      if (child.nodeType === NODE_TYPE.TEXT_NODE) {
+        content += child.nodeValue;
       }
+    }
 
-      let content = "";
-      for (const child of domSymbolTree.childrenIterator(this)) {
-        if (child.nodeType === NODE_TYPE.TEXT_NODE) {
-          content += child.nodeValue;
-        }
-      }
+    evaluateStylesheet.call(this, content, this.sheet, this._ownerDocument.URL);
 
-      evaluateStylesheet.call(this, content, this.sheet, this._ownerDocument.URL);
-    });
+    super._attach();
   }
 }
 

test/jsdom/misc.js
@@ -316,111 +316,6 @@ describe("jsdom/miscellaneous", () => {
     assert.strictEqual(a.getAttributeNode("id").nodeValue, "123", "Attribute stringify");
   });
 
-  specify("mutation_events", () => {
-    const document = jsdom.jsdom();
-    let created = "";
-    let removed = "";
-    document.addEventListener("DOMNodeInserted", ev => {
-      created += ev.target.tagName;
-    });
-    document.addEventListener("DOMNodeRemoved", ev => {
-      removed += ev.target.tagName;
-    });
-    const h1 = document.createElement("h1");
-    const h2 = document.createElement("h2");
-    const h3 = document.createElement("h3");
-
-    document.body.appendChild(h2);
-    document.body.insertBefore(h1, h2);
-    document.body.insertBefore(h3, null);
-    assert.strictEqual("H2H1H3", created, "an event should be dispatched for each created element");
-
-    document.body.removeChild(h1);
-    document.body.insertBefore(h3, h2);
-    assert.strictEqual("H1H3", removed, "an event should be dispatched for each removed element");
-
-    let text = h2.innerHTML = "foo";
-    h2.addEventListener("DOMCharacterDataModified", ev => {
-      text = ev.target.nodeValue;
-    });
-    h2.firstChild.nodeValue = "bar";
-    assert.equal(h2.innerHTML, text, "ChactaterData changes should be captured");
-
-    let event;
-    h2.setAttribute("class", "foo");
-    document.addEventListener("DOMAttrModified", ev => {
-      event = ev;
-    });
-    h2.setAttribute("class", "bar");
-    assert.ok(event, "Changing an attribute should trigger DOMAttrModified");
-    assert.equal(event.attrName, "class", "attrName should be class");
-    assert.equal(event.prevValue, "foo", "prevValue should be foo");
-    assert.equal(event.newValue, "bar", "newValue should be bar");
-
-    event = false;
-    h2.setAttribute("class", "bar");
-    assert.ok(!event, "Setting the same value again should not trigger an event");
-
-    h2.removeAttribute("class");
-    assert.ok(event, "Removing an attribute should trigger DOMAttrModified");
-    assert.equal(event.attrName, "class", "attrName should be class");
-    assert.equal(event.prevValue, "bar", "prevValue should be bar");
-  });
-
-  specify("DomSubtreeModifiedEvents", () => {
-    const document = jsdom.jsdom();
-    let firedAfterAddedChild = false;
-    let firedAfterAddedTextNode = false;
-    let firedAfterAddingAttr = false;
-    let firedAfterChangingAttr = false;
-    let firedAfterRemovedAttr = false;
-
-    document.addEventListener("DOMSubtreeModified", () => {
-      firedAfterAddedChild = true;
-    });
-    const div = document.createElement("div");
-    document.body.appendChild(div);
-    assert.ok(firedAfterAddedChild, "DOMSubtreeModified event should be fired for each created element");
-
-    document.addEventListener("DOMSubtreeModified", () => {
-      firedAfterAddedTextNode = true;
-    });
-    const textNode = document.createTextNode("text node test");
-    document.getElementsByTagName("div")[0].appendChild(textNode);
-    assert.ok(firedAfterAddedTextNode, "DOMSubtreeModified event should be fired when texnode value changed");
-    document.addEventListener("DOMSubtreeModified", () => {
-      firedAfterAddingAttr = true;
-    });
-    document.getElementsByTagName("div")[0].setAttribute("class", "test-class");
-    assert.ok(firedAfterAddingAttr, "DOMSubtreeModified event should be fired when attribute added");
-
-    document.addEventListener("DOMSubtreeModified", () => {
-      firedAfterChangingAttr = true;
-    });
-    document.getElementsByTagName("div")[0].setAttribute("class", "test-class-2");
-    assert.ok(firedAfterChangingAttr, "DOMSubtreeModified event should be fired when attribute value changed");
-
-    firedAfterChangingAttr = false;
-    document.getElementsByTagName("div")[0].setAttribute("class", "test-class-2");
-    assert.ok(
-      firedAfterChangingAttr === false,
-      "DOMSubtreeModified not be fired when new attribute value same as old one"
-    );
-
-    document.addEventListener("DOMSubtreeModified", () => {
-      firedAfterRemovedAttr = true;
-    });
-    document.getElementsByTagName("div")[0].removeAttribute("class");
-    assert.ok(firedAfterRemovedAttr, "DOMSubtreeModified event should be fired when attribute removed");
-
-    firedAfterRemovedAttr = false;
-    document.getElementsByTagName("div")[0].removeAttribute("class");
-    assert.ok(
-      firedAfterRemovedAttr === false,
-      "DOMSubtreeModified not be fired when try to remove attribute does not exists"
-    );
-  });
-
   specify("childNodes_updates_on_insertChild", () => {
     const window = jsdom.jsdom("").defaultView;
     const div = window.document.createElement("div");
@@ -981,7 +876,6 @@ describe("jsdom/miscellaneous", () => {
           features: {
             FetchExternalResources: ["script", "frame", "link"],
             ProcessExternalResources: ["script", "frame", "link"],
-            MutationEvents: "2.0",
             QuerySelector: false
           }
         },

lib/jsdom.js
@@ -146,7 +146,6 @@ exports.jQueryify = exports.jsdom.jQueryify = function (window, jqueryUrl, callb
   const features = implImpl._features;
   implImpl._addFeature("FetchExternalResources", ["script"]);
   implImpl._addFeature("ProcessExternalResources", ["script"]);
-  implImpl._addFeature("MutationEvents", ["2.0"]);
 
   const scriptEl = window.document.createElement("script");
   scriptEl.className = "jsdom";
@@ -309,7 +308,6 @@ function processHTML(config) {
   if (config.scripts.length > 0 || config.src.length > 0) {
     implImpl._addFeature("FetchExternalResources", ["script"]);
     implImpl._addFeature("ProcessExternalResources", ["script"]);
-    implImpl._addFeature("MutationEvents", ["2.0"]);
 
     for (const scriptSrc of config.scripts) {
       const script = window.document.createElement("script");

lib/jsdom/browser/documentfeatures.js
@@ -5,14 +5,12 @@ const idlUtils = require("../living/generated/utils");
 exports.availableDocumentFeatures = [
   "FetchExternalResources",
   "ProcessExternalResources",
-  "MutationEvents",
   "SkipExternalResources"
 ];
 
 exports.defaultDocumentFeatures = {
   FetchExternalResources: ["script", "link"], // omitted by default: "frame"
   ProcessExternalResources: ["script"], // omitted by default: "frame", "iframe"
-  MutationEvents: "2.0",
   SkipExternalResources: false
 };
 

lib/jsdom/level3/xpath.js
@@ -1747,16 +1747,19 @@ module.exports = core => {
     this._value = value;
     this._resultType = resultType;
     this._i = 0;
-    this._invalidated = false;
-    if (this.resultType === XPathResult.UNORDERED_NODE_ITERATOR_TYPE ||
-        this.resultType === XPathResult.ORDERED_NODE_ITERATOR_TYPE) {
-      doc.addEventListener('DOMSubtreeModified', invalidate, true);
-      var self = this;
-      function invalidate() {
-        self._invalidated = true;
-        doc.removeEventListener('DOMSubtreeModified', invalidate, true);
-      }
-    }
+
+    // TODO: we removed mutation events but didn't take care of this. No tests fail, so that's nice, but eventually we
+    // should fix this, preferably by entirely replacing our XPath implementation.
+    // this._invalidated = false;
+    // if (this.resultType === XPathResult.UNORDERED_NODE_ITERATOR_TYPE ||
+    //     this.resultType === XPathResult.ORDERED_NODE_ITERATOR_TYPE) {
+    //   doc.addEventListener('DOMSubtreeModified', invalidate, true);
+    //   var self = this;
+    //   function invalidate() {
+    //     self._invalidated = true;
+    //     doc.removeEventListener('DOMSubtreeModified', invalidate, true);
+    //   }
+    // }
   }
   XPathResult.ANY_TYPE = 0;
   XPathResult.NUMBER_TYPE = 1;

lib/jsdom/living/events/MutationEvent-impl.js
@@ -1,22 +1,0 @@
-"use strict";
-
-const EventImpl = require("./Event-impl").implementation;
-
-class MutationEventImpl extends EventImpl {
-  initMutationEvent(type, bubbles, cancelable, relatedNode, prevValue, newValue, attrName, attrChange) {
-    if (this._dispatchFlag) {
-      return;
-    }
-
-    this.initEvent(type, bubbles, cancelable);
-    this.relatedNode = relatedNode;
-    this.prevValue = prevValue;
-    this.newValue = newValue;
-    this.attrName = attrName;
-    this.attrChange = attrChange;
-  }
-}
-
-module.exports = {
-  implementation: MutationEventImpl
-};

lib/jsdom/living/events/MutationEvent.idl
@@ -1,12 +1,0 @@
-interface MutationEvent : Event {
-    // attrChangeType
-    const unsigned short MODIFICATION = 1;
-    const unsigned short ADDITION = 2;
-    const unsigned short REMOVAL = 3;
-    readonly    attribute Node?          relatedNode;
-    readonly    attribute DOMString      prevValue;
-    readonly    attribute DOMString      newValue;
-    readonly    attribute DOMString      attrName;
-    readonly    attribute unsigned short attrChange;
-    void initMutationEvent (DOMString typeArg, boolean bubblesArg, boolean cancelableArg, Node? relatedNodeArg, DOMString prevValueArg, DOMString newValueArg, DOMString attrNameArg, unsigned short attrChangeArg);
-};

commit 8703bf6517f7cbbfd7e9799a5b3f87933c7fbf10

Don't crash on parsing bad CSS
Fixes #1477. This was a regression in https://github.com/tmpvar/jsdom/commit/780b5dc6de5d739ba10e9bfcb730d535d931f153 since we no longer parse CSS inside mutation events, where errors just go to window.onerror. Instead, bad CSS now fires a jsdomError.

Also cleaned up and consolidated duplicate code in the HTMLLinkElement and HTMLStyleElement implementations.

lib/jsdom/living/helpers/stylesheets.js
@@ -1,0 +1,60 @@
+"use strict";
+const cssom = require("cssom");
+const normalizeEncoding = require("../helpers/encoding").normalizeEncoding;
+const resourceLoader = require("../../browser/resource-loader");
+const resolveHref = require("../../utils").resolveHref;
+
+exports.fetchStylesheet = (elementImpl, url, sheet) => {
+  let defaultEncoding = elementImpl._ownerDocument._encoding;
+  if (elementImpl.localName === "link" && elementImpl.hasAttribute("charset")) {
+    defaultEncoding = normalizeEncoding(elementImpl.getAttribute("charset"));
+  }
+
+  resourceLoader.load(elementImpl, url, { defaultEncoding }, data => {
+    // TODO: MIME type checking?
+    exports.evaluateStylesheet(elementImpl, data, sheet, url);
+  });
+};
+
+exports.evaluateStylesheet = (elementImpl, data, sheet, baseURL) => {
+  let newStyleSheet;
+  try {
+    newStyleSheet = cssom.parse(data);
+  } catch (e) {
+    if (elementImpl._ownerDocument._defaultView) {
+      const error = new Error("Could not parse CSS stylesheet");
+      error.detail = data;
+      elementImpl._ownerDocument._defaultView._virtualConsole.emit("jsdomError", error);
+    }
+
+    elementImpl._ownerDocument.styleSheets.push(sheet);
+    return;
+  }
+
+  const spliceArgs = newStyleSheet.cssRules;
+  spliceArgs.unshift(0, sheet.cssRules.length);
+  Array.prototype.splice.apply(sheet.cssRules, spliceArgs);
+
+  scanForImportRules(elementImpl, sheet.cssRules, baseURL);
+
+  elementImpl._ownerDocument.styleSheets.push(sheet);
+};
+
+function scanForImportRules(elementImpl, cssRules, baseURL) {
+  if (!cssRules) {
+    return;
+  }
+
+  for (let i = 0; i < cssRules.length; ++i) {
+    if (cssRules[i].cssRules) {
+      // @media rule: keep searching inside it.
+      scanForImportRules(elementImpl, cssRules[i].cssRules, baseURL);
+    } else if (cssRules[i].href) {
+      // @import rule: fetch the resource and evaluate it.
+      // See http://dev.w3.org/csswg/cssom/#css-import-rule
+      //     If loading of the style sheet fails its cssRules list is simply
+      //     empty. I.e. an @import rule always has an associated style sheet.
+      exports.fetchStylesheet(elementImpl, resolveHref(baseURL, cssRules[i].href), elementImpl.sheet);
+    }
+  }
+}

lib/jsdom/living/nodes/HTMLLinkElement-impl.js
@@ -1,70 +1,9 @@
 "use strict";
-
-const cssom = require("cssom");
-
 const HTMLElementImpl = require("./HTMLElement-impl").implementation;
 const LinkStyleImpl = require("./LinkStyle-impl").implementation;
 const idlUtils = require("../generated/utils");
-const normalizeEncoding = require("../helpers/encoding").normalizeEncoding;
-
 const resourceLoader = require("../../browser/resource-loader");
-const resolveHref = require("../../utils").resolveHref;
-
-/**
- * @this {core.HTMLLinkElement|core.HTMLStyleElement}
- * @param {string} url
- * @param {cssom.CSSStyleSheet} sheet
- * @see http://dev.w3.org/csswg/cssom/#requirements-on-user-agents-implementing0
- */
-function fetchStylesheet(url, sheet) {
-  resourceLoader.load(this,
-  url,
-  { defaultEncoding: normalizeEncoding(this.getAttribute("charset")) || this._ownerDocument._encoding },
-  data => {
-    // TODO: abort if the content-type is not text/css, and the document is
-    // in strict mode
-    url = sheet.href = resourceLoader.resolveResourceUrl(this.ownerDocument, url);
-    evaluateStylesheet.call(this, data, sheet, url);
-  });
-}
-/**
- * @this {core.HTMLLinkElement|core.HTMLStyleElement}
- * @param {string} data
- * @param {cssom.CSSStyleSheet} sheet
- * @param {string} baseUrl
- */
-function evaluateStylesheet(data, sheet, baseUrl) {
-  // this is the element
-  const newStyleSheet = cssom.parse(data);
-  const spliceArgs = newStyleSheet.cssRules;
-  spliceArgs.unshift(0, sheet.cssRules.length);
-  Array.prototype.splice.apply(sheet.cssRules, spliceArgs);
-  scanForImportRules.call(this, sheet.cssRules, baseUrl);
-  this.ownerDocument.styleSheets.push(sheet);
-}
-/**
- * @this {core.HTMLLinkElement|core.HTMLStyleElement}
- * @param {cssom.CSSStyleSheet} sheet
- * @param {string} baseUrl
- */
-function scanForImportRules(cssRules, baseUrl) {
-  if (!cssRules) {
-    return;
-  }
-
-  for (let i = 0; i < cssRules.length; ++i) {
-    if (cssRules[i].cssRules) {
-      // @media rule: keep searching inside it.
-      scanForImportRules.call(this, cssRules[i].cssRules, baseUrl);
-    } else if (cssRules[i].href) {
-      // @import rule: fetch the resource and evaluate it.
-      // See http://dev.w3.org/csswg/cssom/#css-import-rule
-      //     If loading of the style sheet fails its cssRules list is simply
-      //     empty. I.e. an @import rule always has an associated style sheet.
-      fetchStylesheet.call(this, resolveHref(baseUrl, cssRules[i].href), this.sheet);
-    }
-  }
-}
+const fetchStylesheet = require("../helpers/stylesheets").fetchStylesheet;
 
 class HTMLLinkElementImpl extends HTMLElementImpl {
   _attach() {
@@ -75,7 +14,7 @@ class HTMLLinkElementImpl extends HTMLElementImpl {
       return;
     }
     if (this.href) {
-      fetchStylesheet.call(this, this.href, this.sheet);
+      fetchStylesheet(this, this.href, this.sheet);
     }
 
     super._attach();

lib/jsdom/living/nodes/HTMLStyleElement-impl.js
@@ -1,71 +1,10 @@
 "use strict";
-
-const cssom = require("cssom");
-
 const HTMLElementImpl = require("./HTMLElement-impl").implementation;
 const LinkStyleImpl = require("./LinkStyle-impl").implementation;
 const idlUtils = require("../generated/utils");
-
 const domSymbolTree = require("../helpers/internal-constants").domSymbolTree;
 const NODE_TYPE = require("../node-type");
-const resourceLoader = require("../../browser/resource-loader");
-const resolveHref = require("../../utils").resolveHref;
-
-/**
- * @this {core.HTMLLinkElement|core.HTMLStyleElement}
- * @param {string} url
- * @param {cssom.CSSStyleSheet} sheet
- * @see http://dev.w3.org/csswg/cssom/#requirements-on-user-agents-implementing0
- */
-function fetchStylesheet(url, sheet) {
-  resourceLoader.load(this,
-  url,
-  { defaultEncoding: this._ownerDocument._encoding },
-  data => {
-    // TODO: abort if the content-type is not text/css, and the document is
-    // in strict mode
-    url = sheet.href = resourceLoader.resolveResourceUrl(this.ownerDocument, url);
-    evaluateStylesheet.call(this, data, sheet, url);
-  });
-}
-/**
- * @this {core.HTMLLinkElement|core.HTMLStyleElement}
- * @param {string} data
- * @param {cssom.CSSStyleSheet} sheet
- * @param {string} baseUrl
- */
-function evaluateStylesheet(data, sheet, baseUrl) {
-  // this is the element
-  const newStyleSheet = cssom.parse(data);
-  const spliceArgs = newStyleSheet.cssRules;
-  spliceArgs.unshift(0, sheet.cssRules.length);
-  Array.prototype.splice.apply(sheet.cssRules, spliceArgs);
-  scanForImportRules.call(this, sheet.cssRules, baseUrl);
-  this.ownerDocument.styleSheets.push(sheet);
-}
-/**
- * @this {core.HTMLLinkElement|core.HTMLStyleElement}
- * @param {cssom.CSSStyleSheet} sheet
- * @param {string} baseUrl
- */
-function scanForImportRules(cssRules, baseUrl) {
-  if (!cssRules) {
-    return;
-  }
-
-  for (let i = 0; i < cssRules.length; ++i) {
-    if (cssRules[i].cssRules) {
-      // @media rule: keep searching inside it.
-      scanForImportRules.call(this, cssRules[i].cssRules, baseUrl);
-    } else if (cssRules[i].href) {
-      // @import rule: fetch the resource and evaluate it.
-      // See http://dev.w3.org/csswg/cssom/#css-import-rule
-      //     If loading of the style sheet fails its cssRules list is simply
-      //     empty. I.e. an @import rule always has an associated style sheet.
-      fetchStylesheet.call(this, resolveHref(baseUrl, cssRules[i].href), this.sheet);
-    }
-  }
-}
+const evaluateStylesheet = require("../helpers/stylesheets").evaluateStylesheet;
 
 class HTMLStyleElementImpl extends HTMLElementImpl {
   _attach() {
@@ -80,7 +19,7 @@ class HTMLStyleElementImpl extends HTMLElementImpl {
       }
     }
 
-    evaluateStylesheet.call(this, content, this.sheet, this._ownerDocument.URL);
+    evaluateStylesheet(this, content, this.sheet, this._ownerDocument.URL);
 
     super._attach();
   }

test/jsdom/misc.js
@@ -88,6 +88,10 @@ describe("jsdom/miscellaneous", () => {
     assert.equal(window.a, 1);
   });
 
+  specify("bad <style> tag contents do not cause exceptions (GH-1477)", () => {
+    assert.doesNotThrow(() => jsdom.jsdom("<style>[}]</style>"));
+  });
+
   specify("jquerify_url", { async: true }, t => {
     const jQueryUrl = "http://code.jquery.com/jquery-1.4.4.min.js";
     jsdom.jQueryify(tmpWindow(), jQueryUrl, (window, jQuery) => {