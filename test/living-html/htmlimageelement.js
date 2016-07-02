rosslavni/jsdom

commit 37532c74235c9eb0260bdcf424719c8e3d7b129b

Clean up internal URL management
This reduces our many internal URL management utilities down to just a few: a helper for reflecting URL attributes, and the document base URL stuff. resolveHref and resolveResourceUrl are gone.

In the process, this attempts to reduce the number of redundant URL parses and serializations. There are still some, especially since the resource loading code expects URL strings instead of URL records.

lib/jsdom/living/nodes/HTMLImageElement-impl.js
@@ -3,6 +3,7 @@ const HTMLElementImpl = require("./HTMLElement-impl").implementation;
 const resourceLoader = require("../../browser/resource-loader");
 const conversions = require("webidl-conversions");
 const Canvas = require("../../utils").Canvas;
+const reflectURLAttribute = require("../../utils").reflectURLAttribute;
 
 class HTMLImageElementImpl extends HTMLElementImpl {
   _attrModified(name, value, oldVal) {
@@ -43,7 +44,7 @@ class HTMLImageElementImpl extends HTMLElementImpl {
   }
 
   get src() {
-    return resourceLoader.resolveResourceUrl(this._ownerDocument, this.getAttribute("src"));
+    return reflectURLAttribute(this, "src");
   }
 
   set src(value) {

lib/jsdom/browser/resource-loader.js
@@ -1,6 +1,5 @@
 "use strict";
 
-const resolveHref = require("../utils").resolveHref;
 const parseDataUrl = require("../utils").parseDataUrl;
 const parseContentType = require("../living/helpers/headers").parseContentType;
 const decodeString = require("../living/helpers/encoding").decodeString;
@@ -142,18 +141,6 @@ exports.enqueue = function (element, resourceUrl, callback) {
   };
 };
 
-exports.resolveResourceUrl = function (document, url) {
-  // if getAttribute returns null, there is no href
-  // lets resolve to an empty string (nulls are not expected farther up)
-  if (url === null) {
-    return "";
-  }
-
-  const baseUrl = documentBaseURLSerialized(document);
-  return resolveHref(baseUrl, url);
-};
-
-
 exports.download = function (url, options, callback) {
   const requestOptions = {
     pool: options.pool,
@@ -208,7 +195,7 @@ exports.download = function (url, options, callback) {
   };
 };
 
-exports.load = function (element, url, options, callback) {
+exports.load = function (element, urlString, options, callback) {
   const document = element._ownerDocument;
   const documentImpl = document.implementation;
 
@@ -216,16 +203,12 @@ exports.load = function (element, url, options, callback) {
     return;
   }
 
-  // if getAttribute returns null, there is no href
-  // lets resolve to an empty string (nulls are not expected farther up)
-  const resourceUrl = exports.resolveResourceUrl(document, url);
-
-  if (documentImpl._hasFeature("SkipExternalResources", resourceUrl)) {
+  if (documentImpl._hasFeature("SkipExternalResources", urlString)) {
     return;
   }
 
-  const urlObj = URL.parse(resourceUrl);
-  const enqueued = exports.enqueue(element, resourceUrl, callback);
+  const urlObj = URL.parse(urlString);
+  const enqueued = exports.enqueue(element, urlString, callback);
   const customLoader = document._customResourceLoader;
   const requestManager = document._requestManager;
   const cookieJar = document._cookieJar;

lib/jsdom/living/helpers/stylesheets.js
@@ -2,18 +2,11 @@
 const cssom = require("cssom");
 const normalizeEncoding = require("../helpers/encoding").normalizeEncoding;
 const resourceLoader = require("../../browser/resource-loader");
-const resolveHref = require("../../utils").resolveHref;
+const whatwgURL = require("whatwg-url");
 
-exports.fetchStylesheet = (elementImpl, url, sheet) => {
-  let defaultEncoding = elementImpl._ownerDocument._encoding;
-  if (elementImpl.localName === "link" && elementImpl.hasAttribute("charset")) {
-    defaultEncoding = normalizeEncoding(elementImpl.getAttribute("charset"));
-  }
-
-  resourceLoader.load(elementImpl, url, { defaultEncoding }, data => {
-    // TODO: MIME type checking?
-    exports.evaluateStylesheet(elementImpl, data, sheet, url);
-  });
+exports.fetchStylesheet = (elementImpl, urlString, sheet) => {
+  const parsedURL = whatwgURL.parseURL(urlString);
+  return fetchStylesheetInternal(elementImpl, urlString, parsedURL, sheet)
 };
 
 exports.evaluateStylesheet = (elementImpl, data, sheet, baseURL) => {
@@ -40,6 +33,18 @@ exports.evaluateStylesheet = (elementImpl, data, sheet, baseURL) => {
   elementImpl._ownerDocument.styleSheets.push(sheet);
 };
 
+function fetchStylesheetInternal(elementImpl, urlString, parsedURL, sheet) {
+  let defaultEncoding = elementImpl._ownerDocument._encoding;
+  if (elementImpl.localName === "link" && elementImpl.hasAttribute("charset")) {
+    defaultEncoding = normalizeEncoding(elementImpl.getAttribute("charset"));
+  }
+
+  resourceLoader.load(elementImpl, urlString, { defaultEncoding }, data => {
+    // TODO: MIME type checking?
+    exports.evaluateStylesheet(elementImpl, data, sheet, parsedURL);
+  });
+}
+
 function scanForImportRules(elementImpl, cssRules, baseURL) {
   if (!cssRules) {
     return;
@@ -54,7 +59,17 @@ function scanForImportRules(elementImpl, cssRules, baseURL) {
       // See http://dev.w3.org/csswg/cssom/#css-import-rule
       //     If loading of the style sheet fails its cssRules list is simply
       //     empty. I.e. an @import rule always has an associated style sheet.
-      exports.fetchStylesheet(elementImpl, resolveHref(baseURL, cssRules[i].href), elementImpl.sheet);
+      const parsed = whatwgURL.parseURL(cssRules[i].href, { baseURL });
+      if (parsed === "failure") {
+        const window = elementImpl._ownerDocument._defaultView;
+        if (window) {
+          const error = new Error(`Could not parse CSS @import URL ${cssRules[i].href} relative to base URL ` +
+                                  `"${whatwgURL.serializeURL(baseURL)}"`);
+          _virtualConsole.emit("jsdomError", error);
+        }
+      } else {
+        fetchStylesheetInternal(elementImpl, whatwgURL.serializeURL(parsed), parsed, elementImpl.sheet);
+      }
     }
   }
 }

lib/jsdom/living/nodes/HTMLAppletElement-impl.js
@@ -1,12 +1,11 @@
 "use strict";
 
 const HTMLElementImpl = require("./HTMLElement-impl").implementation;
-
-const resourceLoader = require("../../browser/resource-loader");
+const reflectURLAttribute = require("../../utils").reflectURLAttribute;
 
 class HTMLAppletElementImpl extends HTMLElementImpl {
   get object() {
-    return resourceLoader.resolveResourceUrl(this._ownerDocument, this.getAttribute("object"));
+    return reflectURLAttribute(this, "object");
   }
 
   set object(V) {
@@ -14,7 +13,7 @@ class HTMLAppletElementImpl extends HTMLElementImpl {
   }
 
   get codeBase() {
-    return resourceLoader.resolveResourceUrl(this._ownerDocument, this.getAttribute("codebase"));
+    return reflectURLAttribute(this, "codebase");
   }
 
   set codeBase(V) {

lib/jsdom/living/nodes/HTMLEmbedElement-impl.js
@@ -1,12 +1,11 @@
 "use strict";
 
 const HTMLElementImpl = require("./HTMLElement-impl").implementation;
-
-const resourceLoader = require("../../browser/resource-loader");
+const reflectURLAttribute = require("../../utils").reflectURLAttribute;
 
 class HTMLEmbedElementImpl extends HTMLElementImpl {
   get src() {
-    return resourceLoader.resolveResourceUrl(this._ownerDocument, this.getAttribute("src"));
+    return reflectURLAttribute(this, "src");
   }
 
   set src(value) {

lib/jsdom/living/nodes/HTMLFormElement-impl.js
@@ -7,7 +7,7 @@ const mapper = require("../../utils").mapper;
 const domSymbolTree = require("../helpers/internal-constants").domSymbolTree;
 const createHTMLCollection = require("../../living/html-collection").create;
 const notImplemented = require("../../browser/not-implemented");
-const resourceLoader = require("../../browser/resource-loader");
+const reflectURLAttribute = require("../../utils").reflectURLAttribute;
 
 // http://www.whatwg.org/specs/web-apps/current-work/#category-listed
 const listedElements = /button|fieldset|input|keygen|object|select|textarea/i;
@@ -115,7 +115,7 @@ class HTMLFormElementImpl extends HTMLElementImpl {
       return this._ownerDocument.URL;
     }
 
-    return resourceLoader.resolveResourceUrl(this._ownerDocument, attributeValue);
+    return reflectURLAttribute(this, "action");
   }
 
   set action(V) {

lib/jsdom/living/nodes/HTMLFrameElement-impl.js
@@ -10,6 +10,7 @@ const URL = require("whatwg-url").URL;
 const documentBaseURLSerialized = require("../helpers/document-base-url").documentBaseURLSerialized;
 const getAttributeValue = require("../attributes").getAttributeValue;
 const idlUtils = require("../generated/utils");
+const reflectURLAttribute = require("../../utils").reflectURLAttribute;
 
 function loadFrame(frame) {
   if (frame._contentDocument) {
@@ -157,7 +158,7 @@ class HTMLFrameElementImpl extends HTMLElementImpl {
   }
 
   get src() {
-    return resourceLoader.resolveResourceUrl(this._ownerDocument, this.getAttribute("src"));
+    return reflectURLAttribute(this, "src");
   }
 
   set src(value) {
@@ -165,7 +166,7 @@ class HTMLFrameElementImpl extends HTMLElementImpl {
   }
 
   get longDesc() {
-    return resourceLoader.resolveResourceUrl(this._ownerDocument, this.getAttribute("longdesc"));
+    return reflectURLAttribute(this, "longdesc");
   }
 
   set longDesc(value) {

lib/jsdom/living/nodes/HTMLLinkElement-impl.js
@@ -2,7 +2,7 @@
 const HTMLElementImpl = require("./HTMLElement-impl").implementation;
 const LinkStyleImpl = require("./LinkStyle-impl").implementation;
 const idlUtils = require("../generated/utils");
-const resourceLoader = require("../../browser/resource-loader");
+const reflectURLAttribute = require("../../utils").reflectURLAttribute;
 const fetchStylesheet = require("../helpers/stylesheets").fetchStylesheet;
 
 class HTMLLinkElementImpl extends HTMLElementImpl {
@@ -25,7 +25,7 @@ class HTMLLinkElementImpl extends HTMLElementImpl {
   }
 
   get href() {
-    return resourceLoader.resolveResourceUrl(this._ownerDocument, this.getAttribute("href"));
+    return reflectURLAttribute(this, "href");
   }
 
   set href(value) {

commit 73bf89f5516efb5cf1990f2b87478a0e688d376b

Fix loading of external images (with canvas package installed)
Regressed in https://github.com/tmpvar/jsdom/commit/37532c74235c9eb0260bdcf424719c8e3d7b129b (v9.2.1). Fixes #1536.

lib/jsdom/living/nodes/HTMLImageElement-impl.js
@@ -20,7 +20,7 @@ class HTMLImageElementImpl extends HTMLElementImpl {
           };
         }
         this._currentSrc = null;
-        resourceLoader.load(this, value, {}, (data, url, response) => {
+        resourceLoader.load(this, this.src, {}, (data, url, response) => {
           if (response && response.statusCode !== undefined && response.statusCode !== 200) {
             throw new Error("Status code: " + response.statusCode);
           }

test/living-html/htmlimageelement.js
@@ -1,6 +1,7 @@
 "use strict";
 const fs = require("fs");
 const path = require("path");
+const http = require("http");
 const jsdom = require("../..");
 const isCanvasInstalled = require("../util").isCanvasInstalled;
 const toFileUrl = require("../util").toFileUrl(__dirname);
@@ -44,6 +45,39 @@ exports["loading image from valid external URL"] = t => {
     t.ok(false, "onerror should not be triggered when loading from valid URL");
     t.done();
   };
+};
+
+exports["loading images should work with relative URLs (GH-1536)"] = t => {
+  if (!isCanvasInstalled(t)) {
+    return;
+  }
+
+  let requestsSoFar = 0;
+
+  const server = http.createServer((request, response) => {
+    if (requestsSoFar === 0) {
+      t.strictEqual(request.url, "/test.html");
+      response.end(`<img src="/test.jpg">`);
+    } else {
+      t.strictEqual(request.url, "/test.jpg");
+      response.end(``);
+      server.close();
+      t.done();
+    }
+
+    ++requestsSoFar;
+  })
+  .listen();
+
+  jsdom.env({
+    url: `http://127.0.0.1:${server.address().port}/test.html`,
+    features: {
+      FetchExternalResources: ["img"]
+    },
+    done(err) {
+      t.ifError(err);
+    }
+  });
 };
 
 exports["loading image from data URL"] = t => {