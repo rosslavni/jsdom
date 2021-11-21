rosslavni/jsdom

commit 1f5acaee40a19ae577df3f50e434acd6f50e1af7

Major performance improvements in some cases
* Improve _insert performance from O(n^2) to O(n):

  "n" here refers to the tree depth of the node being inserted, so lots of nested elements causes insertion to be extremely slow. Deep cloning, for example, calls _insert O(n) times, which makes cloning O(n^3) without this optimization and O(n^2) with it.

  It works by only checking isConnected (which is O(n)) once on the node being inserted rather than checking it on every single descendant of the node being inserted. Checking isConnected for every descendant isn't necessary since any node should have the same isConnected value as any other node in the same node tree.

* Improve contains performance:

  The previous implementation of a node's contains method uses compareDocumentPosition, which runs a lot of extra features that aren't used by contains. In some measurements this reduces the time spent from 560 ms down to 30 ms. The new implementation works by simply checking each ancestor of the other node until it finds the this node (as in this.contains(other)), rather than checking a number of relationships between the two nodes but utilizing only one of them.

Also removes "obsolete node type" errors from compareDocumentPosition(), since those were removed from the spec.

lib/jsdom/living/nodes/Node-impl.js
@@ -11,7 +11,7 @@ const { setAnExistingAttributeValue } = require("../attributes");
 
 const NodeList = require("../generated/NodeList");
 
-const { nodeRoot, nodeLength } = require("../helpers/node");
+const { nodeRoot, nodeLength, isInclusiveAncestor } = require("../helpers/node");
 const { domSymbolTree } = require("../helpers/internal-constants");
 const { documentBaseURLSerialized } = require("../helpers/document-base-url");
 const { queueTreeMutationRecord } = require("../helpers/mutation-observers");
@@ -20,13 +20,6 @@ const {
   isShadowRoot, shadowIncludingRoot, assignSlot, assignSlotableForTree, assignSlotable, signalSlotChange, isSlot,
   shadowIncludingInclusiveDescendantsIterator, shadowIncludingDescendantsIterator
 } = require("../helpers/shadow-dom");
-
-function isObsoleteNodeType(node) {
-  return node.nodeType === NODE_TYPE.ENTITY_NODE ||
-    node.nodeType === NODE_TYPE.ENTITY_REFERENCE_NODE ||
-    node.nodeType === NODE_TYPE.NOTATION_NODE ||
-    node.nodeType === NODE_TYPE.CDATA_SECTION_NODE;
-}
 
 function nodeEquals(a, b) {
   if (a.nodeType !== b.nodeType) {
@@ -375,10 +368,6 @@ class NodeImpl extends EventTargetImpl {
     let node1 = other;
     let node2 = this;
 
-    if (isObsoleteNodeType(node2) || isObsoleteNodeType(node1)) {
-      throw new Error("Obsolete node type");
-    }
-
     let attr1 = null;
     let attr2 = null;
 
@@ -464,12 +453,7 @@ class NodeImpl extends EventTargetImpl {
   }
 
   contains(other) {
-    if (other === null) {
-      return false;
-    } else if (this === other) {
-      return true;
-    }
-    return Boolean(this.compareDocumentPosition(other) & NODE_DOCUMENT_POSITION.DOCUMENT_POSITION_CONTAINED_BY);
+    return isInclusiveAncestor(this, other);
   }
 
   isEqualNode(node) {
@@ -815,6 +799,8 @@ class NodeImpl extends EventTargetImpl {
       domSymbolTree.previousSibling(childImpl) :
       domSymbolTree.lastChild(this);
 
+    let isConnected;
+
     for (const node of nodesImpl) {
       if (!childImpl) {
         domSymbolTree.appendChild(this, node);
@@ -851,8 +837,12 @@ class NodeImpl extends EventTargetImpl {
 
       this._descendantAdded(this, node);
 
-      for (const inclusiveDescendant of shadowIncludingInclusiveDescendantsIterator(node)) {
-        if (inclusiveDescendant.isConnected) {
+      if (isConnected === undefined) {
+        isConnected = nodeImpl.isConnected;
+      }
+
+      if (isConnected) {
+        for (const inclusiveDescendant of shadowIncludingInclusiveDescendantsIterator(node)) {
           if (inclusiveDescendant._ceState === "custom") {
             enqueueCECallbackReaction(inclusiveDescendant, "connectedCallback", []);
           } else {

commit 056920b9e64dfdb1de706e9934fb5733a71c259e

Fix connectedCallback with document fragments
This regressed in 1f5acaee40a19ae577df3f50e434acd6f50e1af7. Closes #3290.

Co-authored-by: Domenic Denicola <d@domenic.me>

lib/jsdom/living/nodes/Node-impl.js
@@ -838,7 +838,7 @@ class NodeImpl extends EventTargetImpl {
       this._descendantAdded(this, node);
 
       if (isConnected === undefined) {
-        isConnected = nodeImpl.isConnected;
+        isConnected = node.isConnected;
       }
 
       if (isConnected) {

test/web-platform-tests/to-upstream/custom-elements/connected-callbacks-template.html
@@ -1,0 +1,33 @@
+<!DOCTYPE html>
+<meta charset="utf-8">
+<script src="/resources/testharness.js"></script>
+<script src="/resources/testharnessreport.js"></script>
+<!-- Regression test for https://github.com/jsdom/jsdom/issues/3290 -->
+
+<body>
+<script>
+"use strict";
+
+test(() => {
+  let innerConnectedCallbackCalled = false;
+  customElements.define("inner-element", class extends HTMLElement {
+    connectedCallback() {
+      innerConnectedCallbackCalled = true;
+    }
+  });
+
+  let outerConnectedCallbackCalled = false;
+  customElements.define("outer-element", class extends HTMLElement {
+    connectedCallback() {
+      const template = document.createElement("template");
+      template.innerHTML = "<inner-element></inner-element>";
+      this.appendChild(document.importNode(template.content, true));
+      outerConnectedCallbackCalled = true;
+    }
+  });
+
+  document.body.appendChild(document.createElement("outer-element"));
+  assert_true(innerConnectedCallbackCalled, "inner connectedCallback must be called");
+  assert_true(outerConnectedCallbackCalled, "outer connectedCallback must be called");
+}, "Nested custom element connectedCallback insertion involving a template DocumentFragment");
+</script>